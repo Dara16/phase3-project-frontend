70b5a49eab8af477ddee622c8e672023a854039e
dara <just4oludee@yahoo.com>
2021-08-14 10:29:56 -0400

    navbar and link created

--- property-app/src/App.js
+++ property-app/src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import TenementsContainer from './components/Tenements/TenementsContainer';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./components/HomePage.js";
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
-    <div className="App">
-      <TenementsContainer />
-    </div>
+    <Router>
+      <div className="App">
+        <div className="app-background" />
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <HomePage/>
+          </Route>
+          <Route exact path="/tenements">
+            <TenementsContainer />
+          </Route>
+          
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ property-app/src/components/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Homepage() {
+    return (
+        <div>
+            <h2 className="home-page-header">CONSTELLATION PROPERTIES</h2>
+            <p>Property Management</p>
+
+        </div>
+    )
+}
